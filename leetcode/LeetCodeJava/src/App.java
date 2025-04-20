4e2c81837b54d76ff38d5895e74d9cbbc8c9e511
Pravesh Gupta <praveshgupta1993@gmail.com>
2025-04-19 21:24:18 -0400

    C-2504192123 Added leetcode java project

---
+++ leetcode/LeetCodeJava/src/App.java
@@ -0,0 +1,11 @@
+package leetcode.LeetCodeJava.src;
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String abc = "abc";
+
+        System.out.println(abc);
+
+        System.out.println("Hello, World 2 You !");
+    }
+}
